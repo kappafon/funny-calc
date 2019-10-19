551d0a08e2e19de1123923166c2790c9434a3d74
kappafon <goran.rihter@gmail.com>
2019-10-19 13:39:04 +0200

    Updates

---
+++ precache-manifest.d59d860af3f18daa46183d656c44e9c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab90d5597058cb99e19ac211bb81300f",
+    "url": "/funny-calc/index.html"
+  },
+  {
+    "revision": "dddcc98ebed229024003",
+    "url": "/funny-calc/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "896c2b2390c88829a586",
+    "url": "/funny-calc/static/js/2.2d157b36.chunk.js"
+  },
+  {
+    "revision": "dddcc98ebed229024003",
+    "url": "/funny-calc/static/js/main.d4ee4085.chunk.js"
+  },
+  {
+    "revision": "ff01fe12bb4e67e25ed7",
+    "url": "/funny-calc/static/js/runtime-main.1a3a6ff4.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/funny-calc/static/media/logo.25bf045c.svg"
+  }
+]);
