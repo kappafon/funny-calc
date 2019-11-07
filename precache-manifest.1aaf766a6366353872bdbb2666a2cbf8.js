680ac77781899696cc4d42190eb246d86ce11707
kappafon <goran.rihter@gmail.com>
2019-11-07 12:46:31 +0100

    Updates

--- precache-manifest.1aaf766a6366353872bdbb2666a2cbf8.js
+++ precache-manifest.1aaf766a6366353872bdbb2666a2cbf8.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "339da733a77baf1e5bec9ca7f4e30912",
+    "revision": "59c8127945f52fad69112fe5c7e41740",
     "url": "/funny-calc/index.html"
   },
   {
-    "revision": "9b4b8bb083806dddfaec",
-    "url": "/funny-calc/static/css/main.fbd2b014.chunk.css"
+    "revision": "622cd890ceddccc3bca8",
+    "url": "/funny-calc/static/css/main.4cbbaeb5.chunk.css"
   },
   {
     "revision": "77b54e44f07276cb7997",
     "url": "/funny-calc/static/js/2.131664ac.chunk.js"
   },
   {
-    "revision": "9b4b8bb083806dddfaec",
-    "url": "/funny-calc/static/js/main.a459d554.chunk.js"
+    "revision": "622cd890ceddccc3bca8",
+    "url": "/funny-calc/static/js/main.dceba5f7.chunk.js"
   },
   {
     "revision": "ff01fe12bb4e67e25ed7",
